b5765295d3ad34d830501bbe13a8888ae2c6b7d3
NoReply133 <47855886+NoReply133@users.noreply.github.com>
2020-05-16 06:19:32 +0200

    Add exercise 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        var heightmap = Resources.Load<Texture2D>("Textures/Heightmap_16");
+        string[] prefabNames = { "Water", "Sand", "Grass", "Forest", "Stone", "Mountain" };
+
+        for(int x=1;x<=heightmap.width;x++)
+        {
+            for(int z=1;z<=heightmap.height;z++)
+            {
+                var pixelColor = heightmap.GetPixel(x, z);
+                var colorValue = Math.Max(pixelColor.r, Math.Max(pixelColor.g, pixelColor.b));
+                string name = prefabNames[(int) Math.Ceiling(colorValue*5)];
+                GameObject tile = Instantiate(Resources.Load("Prefabs/" + name + "Tile"), new Vector3(z*8,colorValue*50,x*10+(z%2==0 ? 0 : 5)), Quaternion.identity) as GameObject;
+                tile.name = name;
+            }
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,15 +4,96 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
+    private bool rightClickOn = false;
+    private int maxPos = 160;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera.main.transform.position = new Vector3(maxPos, 80, 80);
     }
 
     // Update is called once per frame
     void Update()
     {
+        handLeftClick();
+        handleZoom();
+        handleXZMovement();
+    }
+
+    void handLeftClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+
+    void handleZoom()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (Camera.main.fieldOfView > 1)
+            {
+                Camera.main.fieldOfView--;
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (Camera.main.fieldOfView < 100)
+            {
+                Camera.main.fieldOfView++;
+            }
+        }
+    }
+
+    void handleXZMovement()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            rightClickOn = true;
+        }
         
+        if (Input.GetMouseButtonUp(1))
+        {
+            rightClickOn = false;
+        }
+
+        if (rightClickOn)
+        {
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                if (Camera.main.transform.position.z > 0)
+                {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z-1);
+                }
+            }
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                if (Camera.main.transform.position.z < maxPos)
+                {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z+1);
+                }
+            }
+            if (Input.GetAxis("Mouse Y") > 0)
+            {
+                if (Camera.main.transform.position.y > 0)
+                {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y-1, Camera.main.transform.position.z);
+                }
+            }
+            if (Input.GetAxis("Mouse Y") < 0)
+            {
+                if (Camera.main.transform.position.y < maxPos)
+                {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y+1, Camera.main.transform.position.z);
+                }
+            }
+        }
     }
 }
