0a690e83738669ce7f3bd93a0b46dc4259387a2b
Joachim <3schmidb@informatik.uni-hamburg.de>
2020-06-21 20:38:48 +0200

    Worker finished

--- Assets/Scripts/Worker.cs
+++ Assets/Scripts/Worker.cs
@@ -11,22 +11,51 @@ public class Worker : MonoBehaviour
 
     public float _age; // The age of this worker
     public float _happiness; // The happiness of this worker
-    private float _timer;
+    private float _timerAge;
+    private float _timerFish;
+    private float _timerClothes;
+    private float _timerSchnapps;
+
+
+    private float _consumesFish = 0.5f ; // period in seconds
+    private float _consumesClothes = 5f ; // period in seconds
+    private float _consumesSchnapps = 0.5f ; // period in seconds
 
     // Start is called before the first frame update
     void Start()
     {
-        _age=0.0f;
+        _age=0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _timer += Time.deltaTime;
-        if (_timer >= 15){
-        _timer = 0.0f;
+        float delta = Time.deltaTime;
+        _timerAge += delta;
+        _timerFish += delta;
+        _timerClothes += delta;
+        _timerSchnapps += delta;
+
+        if (_timerAge >= 15){
+        _timerAge = 0.0f;
         _age+=1f;
         Age();
+        };
+
+        if (_timerFish >= _consumesFish){
+        _timerFish = 0.0f;
+         consum(GameManager.ResourceTypes.Fish);
+        };
+
+
+        if (_timerClothes >= _consumesClothes){
+        _timerClothes = 0.0f;
+         consum(GameManager.ResourceTypes.Clothes);
+        };
+
+        if (_timerSchnapps >= _consumesSchnapps){
+        _timerSchnapps = 0.0f;
+         consum(GameManager.ResourceTypes.Schnapps);
         };
     }
 
@@ -66,6 +95,25 @@ public class Worker : MonoBehaviour
 
     private void Die()
     {
-        Destroy(this.gameObject, 1f);
+        Destroy(this.gameObject, 1);
+    }
+
+    private void consum(GameManager.ResourceTypes res)
+    {
+        if (_gameManager.HasResourceInWarehoues(res)) 
+        {
+            _gameManager.RemoveResourceFromWarehouse(res, 1);
+            happy(true);
+        }
+        else {
+            happy(false);
+        };
+    }
+
+    private void happy(bool mood)
+    {
+        if (mood) { _happiness += 1; } else { _happiness -= 1 ;};
+        if (_happiness < 0) {_happiness = 0 ;};
+        if (_happiness > 100) {_happiness = 100;}; 
     }
 }
